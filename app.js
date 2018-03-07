a3dd0e1b849db08f8e3a049ccf7b688c85b4e7e2
William Allain <wallain@twimm.fr>
2018-03-07 12:42:37 +0100

    [1rst Commit] Commit before settings twitter params

---
+++ app.js
@@ -0,0 +1,6 @@
+var Twitter = require('twitter');
+var config = require('./config.js');
+
+var T = new Twitter(config);
+
+// Setting up the tweet search core
---
+++ config.js
@@ -0,0 +1,8 @@
+//Config JS
+
+module.exports = {
+    consumer_key: 'Gi9tV2Yj28G79R2HfXe5mwR7r',
+    consumer_secret: 'krd1cmOkuerrap2ux3kOzQ8IcWjXAP6oKlvqJTDxajADjAEsa8',
+    access_token_key: 'VgaHTbD5ay0LxZX09XRFw6TuFHpqgtYqvri1bj3',
+    access_token_secret: 'yOJz63vrBE2HTUDMNyhqIwM9NjXFrKcP9TVMnGB2q8m13'
+}
