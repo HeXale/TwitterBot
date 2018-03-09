f65f013399deef7afdd26ea0f66d24e8c4caf562
William Allain <wallain@twimm.fr>
2018-03-09 12:43:18 +0100

    [09/03/2018] NPM INSTALL

--- app.js
+++ app.js
@@ -18,17 +18,17 @@ T.get('search/tweets', params, function(err, data, response) {
         for (let i=0; i < data.statuses.length; i++) {
             let id = {id: data.statuses[i].id_str};
         }
-        T.post('favorites/create', id, function(err, response))
+        T.post('favorites/create', id, function(err, response)
         {
             if (err) {
-                console.log(err[0]."It appears that Johnny wasn't there")
+                console.log(err[0].message)
             } else {
                 let username = response.user_screen_name;
                 let tweetId = response.id_str;
 
-                console.log('Faved :'; 'https://twitter.com/${username}/status/${tweetId}')
+                console.log('Favorited : ', 'https://twitter.com/${username}/status/${tweetId}')
             }
-        }
+        })
     } else {
         console.log("There might be a small mistake somewhere...")
     }
