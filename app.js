ab7be7a1d1132dc3b6702574ac828ebfbeab6166
William Allain <wallain@twimm.fr>
2018-03-09 12:15:38 +0100

    [09/03/2018] Mise en place de la structure du bot tweet avec les premiers params

--- app.js
+++ app.js
@@ -1,7 +1,31 @@
-var Twitter = require('twitter');
-var config = require('./config.js');
+let Twitter = require('twitter');
+let config = require('./config.js');
 
-var T = new Twitter(config);
+let T = new Twitter(config);
 
 // Setting up the tweet search core
-//kl;sadgbkasdfg df
+
+let params = {
+    q: "@hexxalle",
+    count: 10,
+    result_type: 'recent',
+    lang: 'fr'
+}
+
+T.get('search/tweets', params, function(err, data, response) {
+    if (!err){
+        //Get the ID of the tweet as result
+        for (let i=0; i < data.statuses.length; i++) {
+            let id = {id: data.statuses[i].id_str};
+        }
+        T.post('favorites/create', id, function(err, response)) {
+            if (!err) {
+                // Here comes Johnny
+            } else {
+                console.log("It appears that Johnny wasn't there")
+            }
+        }
+    } else {
+        console.log("There might be a small mistake somewhere...")
+    }
+})
